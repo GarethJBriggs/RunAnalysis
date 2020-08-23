d15486264a69fd3ac5eb1be6e9f948e9a4bcf233
Gareth Briggs <garethjamesbriggs@gmail.com>
2020-08-23 13:16:31 +0100

    Update run_analysis.R
    Duplicate nameing of labels seperated to labels and subjects

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,7 @@
                 train_ds <- read.table("X_train.txt")
                 test_ds <- read.table("X_test.txt")
-                train_labels <- read.table("y_train,txt")
+                train_labels <- read.table("y_train.txt")
                 test_labels <-read.table("y_test.txt")
-                train_labels <- read.table("subject_train.txt")
-                test_labels <- read.table("subject_test.txt")
+                train_subjects <- read.table("subject_train.txt")
+                test_subjects <- read.table("subject_test.txt")
                 
