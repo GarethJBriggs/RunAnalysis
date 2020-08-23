177468d3cd2c702c067b2aef5a2b1683af6b9c33
Gareth Briggs <garethjamesbriggs@gmail.com>
2020-08-23 13:07:56 +0100

    Update run_analysis.R
    Completed .txt extensions for read files

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,7 @@
-        train_ds <- read.table("X_train.txt")
-        test_ds <- read.table("X_test.txt")
-        train_labels <- read.table("y_train")
-        test_labels <-read.table("y_test")
-        train_labels <- read.table("subject_train.txt")
-        test_labels <- read.table("subject_test.txt")
-        
+                train_ds <- read.table("X_train.txt")
+                test_ds <- read.table("X_test.txt")
+                train_labels <- read.table("y_train,txt")
+                test_labels <-read.table("y_test.txt")
+                train_labels <- read.table("subject_train.txt")
+                test_labels <- read.table("subject_test.txt")
+                
