ce7056be31fe0a7478815b03de3e621599d2649b
Gareth Briggs <garethjamesbriggs@gmail.com>
2020-08-23 15:54:51 +0100

    Create ds_561_rename.R
    First attempt at running 561 vars

---
+++ ds_561_rename.R
@@ -0,0 +1,21 @@
+rename_(pilot_df, "tBodyAcc-mean()-X" = V1, "tBodyAcc-mean()-Y" = V2, "tBodyAcc-mean()-Z" = V3, "tBodyAcc-std()-X" = V4, "tBodyAcc-std()-Y" = V5, 
+       "tBodyAcc-std()"-Z = V6, "tBodyAcc-mad()-X" = V7, "tBodyAcc-mad()-Y" = V8, "tBodyAcc-mad()-Z" = V9, "tBodyAcc-max()-X" = V10, "tBodyAcc-max()-Y" = V11, 
+       tBodyAcc-max()-Z = V12,
+       tBodyAcc-min()-X = V13, tBodyAcc-min()-Y = V14, tBodyAcc-min()-Z = V15, tBodyAcc-sma() = V16, tBodyAcc-energy()-X = V17, tBodyAcc-energy()-Y = V18,
+       tBodyAcc-energy()-Z = V19, tBodyAcc-iqr()-X = V20, tBodyAcc-iqr()-Y = V21, tBodyAcc-iqr()-Z = V22, tBodyAcc-entropy()-X = V23, tBodyAcc-entropy()-Y = V24,
+       tBodyAcc-entropy()-Z = V25, tBodyAcc-arCoeff()-X,1 = V26, tBodyAcc-arCoeff()-X,2 = V27, tBodyAcc-arCoeff()-X,3 = V28, tBodyAcc-arCoeff()-X,4 = V29,
+       tBodyAcc-arCoeff()-Y,1 = V30, tBodyAcc-arCoeff()-Y,2 = V31, tBodyAcc-arCoeff()-Y,3 = V32, tBodyAcc-arCoeff()-Y,4 = V33, tBodyAcc-arCoeff()-Z,1 = V34,
+       tBodyAcc-arCoeff()-Z,2 = V35, tBodyAcc-arCoeff()-Z,3 = V36, tBodyAcc-arCoeff()-Z,4 = V37, tBodyAcc-correlation()-X,Y = V38, tBodyAcc-correlation()-X,Z 
+       = V39, tBodyAcc-correlation()-Y,Z = V40, tGravityAcc-mean()-X = V41, tGravityAcc-mean()-Y = V42, tGravityAcc-mean()-Z = V43, tGravityAcc-std()-X = V44,
+       tGravityAcc-std()-Y = V45, tGravityAcc-std()-Z = V46, tGravityAcc-mad()-X = V47, tGravityAcc-mad()-Y = V48, tGravityAcc-mad()-Z = V49, tGravityAcc-max()-X
+       = V50, tGravityAcc-max()-Y = V51, tGravityAcc-max()-Z = V52, tGravityAcc-min()-X = V53, tGravityAcc-min()-Y = V54, tGravityAcc-min()-Z = V55,
+       tGravityAcc-sma() = V56, tGravityAcc-energy()-X = V57, tGravityAcc-energy()-Y = V58, tGravityAcc-energy()-Z = V59, tGravityAcc-iqr()-X = V60, 
+       tGravityAcc-iqr()-Y = V61, tGravityAcc-iqr()-Z = V62, tGravityAcc-entropy()-X = V63, tGravityAcc-entropy()-Y = V64, tGravityAcc-entropy()-Z = V65,
+       tGravityAcc-arCoeff()-X,1 = V66, tGravityAcc-arCoeff()-X,2 = V67, tGravityAcc-arCoeff()-X,3 = V68, tGravityAcc-arCoeff()-X,4 = V69, tGravityAcc-arCoeff()-Y,1
+       = V70, tGravityAcc-arCoeff()-Y,2 = V71, tGravityAcc-arCoeff()-Y,3 = V72, tGravityAcc-arCoeff()-Y,4 = V73, tGravityAcc-arCoeff()-Z,1 = V74, 
+       tGravityAcc-arCoeff()-Z,2 = V75, tGravityAcc-arCoeff()-Z,3 = V76, tGravityAcc-arCoeff()-Z,4 = V77, tGravityAcc-correlation()-X,Y = V78, 
+       tGravityAcc-correlation()-X,Z = V79, tGravityAcc-correlation()-Y,Z = V80, tBodyAccJerk-mean()-X = V81, tBodyAccJerk-mean()-Y = V82, tBodyAccJerk-mean()-Z
+       = V83, tBodyAccJerk-std()-X = V84, tBodyAccJerk-std()-Y = V85, tBodyAccJerk-std()-Z = V86, tBodyAccJerk-mad()-X = V87, tBodyAccJerk-mad()-Y = V88,
+       tBodyAccJerk-mad()-Z = V89, tBodyAccJerk-max()-X = V90, tBodyAccJerk-max()-Y = V91, tBodyAccJerk-max()-Z = V92, tBodyAccJerk-min()-X = V93, 
+       tBodyAccJerk-min()-Y = V94, tBodyAccJerk-min()-Z = V95, tBodyAccJerk-sma() = V96, tBodyAccJerk-energy()-X = V97, tBodyAccJerk-energy()-Y = V98,
+       tBodyAccJerk-energy()-Z = V99, tBodyAccJerk-iqr()-X = V100)
