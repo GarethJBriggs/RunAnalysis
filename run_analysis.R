9cb9b0661a1f7561427263597d7a2767f7f45941
Gareth Briggs <garethjamesbriggs@gmail.com>
2020-08-27 15:27:39 +0100

    Update run_analysis.R
    Clarified object names. Labels => activities. Tested code. Produces subjects, activities cols + 33 mean, 33 std cols, to be named.

--- run_analysis.R
+++ run_analysis.R
@@ -9,25 +9,25 @@ library(dplyr)
         ## merges the primary test and train data sets
         ds_df <- rbind(test_ds, train_ds)
         ## merges the activity labels
-        labels_col <- rbind(test_labels, train_labels)
-        labels_col <- rename(labels_col, activity = V1)
+        activities_col <- rbind(test_labels, train_labels)
+        ## names the activities column
+        activities_col <- rename(activities_col, activity = V1)
         ## merges the subjects
         subjects_col<- rbind(test_subjects, train_subjects)
-        ## labels the subjects column
+        ## names the subjects column
         subjects_col <- rename(subjects_col, subject = V1)
         ## merges the subject and labels columns together
-        sub_lab_df <- cbind(subjects_col, labels_col)
+        sub_act_df <- cbind(subjects_col, activities_col)
         ## reads the features file
         features <- read.table("features.txt")
         ## uses regular expressions to filter the std and mean data labels and column number 
         old_names <- features[grep('[Ss]td\\(\\)|[Mm]ean\\(\\)', features$V2),]
         ## prints out old_names df
         old_names
-        ## subsets old names to isolate column numbers where std and mean occur
-        old_names_sub <- old_names[,1]
         ##subsets test and train primary dataset to the columns contains mean and std data
-        ds_sub <- ds_df[, old_names_sub]
-        ## creates a dataframe containing subjects, activity and primary data columns
-        merge_df <- cbind(sub_lab_df, ds_sub)
+        ds_sub <- ds_df[, old_names$V1]
+        ## creates a df containing subjects, activity and primary data columns
+        raw_mge_df <- cbind(sub_act_df, ds_sub)
+        raw_mge_df        
                 
-                
+        
