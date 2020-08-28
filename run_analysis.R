55d376f8d160d2b1cf9983102debaa28815a4190
Gareth Briggs <garethjamesbriggs@gmail.com>
2020-08-28 15:47:23 +0100

    Update run_analysis.R
    Tidied and checked run analysis. Final df is coerced from a tibble.

--- run_analysis.R
+++ run_analysis.R
@@ -29,11 +29,81 @@ library(dplyr)
         ## codes for the activities variables
         raw_mge_df$activity <- recode(raw_mge_df$activity, "1" = "walking", 
         "2" = "walkingUpstairs", "3" = "walkingDownstairs", "4" = "sitting", 
-        "5" = "standing", "6" = "laying" )    
+        "5" = "standing", "6" = "laying" )
+        ## renames 66 mean and std column variables
+        raw_mge_df <- rename(raw_mge_df, timeBodyAccelerationXaxisMean = V1, 
+                             timeBodyAccelerationYaxisMean = V2, 
+                             timeBodyAccelerationZaxisMean = V3,
+                             timeBodyAccelerationXaxisStd = V4,
+                             timeBodyAccelerationYaxisStd = V5,
+                             timeBodyAccelerationZaxisStd = V6,
+                             timeGravityAccelerationXaxisMean = V41,
+                             timeGravityAccelerationYaxisMean = V42,
+                             timeGravityAccelerationZaxisMean = V43,
+                             timeGravityAccelerationXaxisStd = V44,
+                             timeGravityAccelerationYaxisStd = V45,
+                             timeGravityAccelerationZaxisStd = V46,
+                             timeBodyAccelerationJerkXaxisMean = V81,
+                             timeBodyAccelerationJerkYaxisMean = V82,
+                             timeBodyAccelerationJerkZaxisMean = V83,
+                             timeBodyAccelerationJerkXaxisStd = V84,
+                             timeBodyAccelerationJerkYaxisStd = V85,
+                             timeBodyAccelerationJerkZaxisStd = V86,
+                             timeBodyGyroscopeXaxisMean = V121,
+                             timeBodyGyroscopeYaxisMean = V122,
+                             timeBodyGyroscopeZaxisMean = V123,
+                             timeBodyGyroscopeXaxisStd = V124,
+                             timeBodyGyroscopeYaxisStd = V125,
+                             timeBodyGyroscopeZaxisStd = V126,
+                             timeBodyGyroscopeJerkXaxisMean = V161,
+                             timeBodyGyroscopeJerkYaxisMean = V162,
+                             timeBodyGyroscopeJerkZaxisMean = V163,
+                             timeBodyGyroscopeJerkXaxisStd = V164,
+                             timeBodyGyroscopeJerkYaxisStd = V165,
+                             timeBodyGyroscopeJerkZaxisStd = V166,
+                             timeBodyAccelerationMagnitudeMean = V201,
+                             timeBodyAccelerationMagnitudeStd = V202,
+                             timeGravityAccelaerationMagnitudeMean = V214,
+                             timegravityaccelerationmagnitudeStd = V215,
+                             timeBodyAccelerationJerkMagnitudeMean = V227,
+                             timeBodyAccelerationJerkMagnitudeStd = V228,
+                             timeBodyGyroscopeMagnitudeMean = V240,
+                             timeBodyGyroscopeMagnitudeStd = V241,
+                             timeBodyGyroscopeJerkMagnitudeMean = V253,
+                             timeBodyGyroscopeJerkMagnitudeStd = V254,
+                             frequencyBodyAccelerationXaxisMean = V266,
+                             frequencyBodyAccelerationYaxisMean = V267,
+                             frequencyBodyAccelerationZaxisMean = V268,
+                             frequencyBodyAccelerationXaxisStd = V269,
+                             frequencyBodyAccelerationYaxisStd = V270,
+                             frequencyBodyAccelerationZaxisStd = V271,
+                             frequencyBodyAccelerationJerkXaxisMean = V345,
+                             frequencyBodyAccelerationJerkYaxisMean = V346,
+                             frequencyBodyAccelerationJerkZaxisMean = V347,
+                             frequencyBodyAccelerationJerkXaxisStd = V348,
+                             frequencyBodyAccelerationJerkYaxisStd = V349,
+                             frequencyBodyAccelerationJerkZaxisStd = V350,
+                             frequencyBodyGyroscopeXaxisMean = V424,
+                             frequencyBodyGyroscopeYaxisMean = V425,
+                             frequencyBodyGyroscopeZaxisMean = V426,
+                             frequencyBodyGyroscopeXaxisStd = V427,
+                             frequencyBodyGyroscopeYaxisStd = V428,
+                             frequencyBodyGyroscopeZaxisStd = V429,
+                             frequencyBodyAccelerationMagnitudeMean = V503,
+                             frequencyBodyAccelerationMagnitudeStd = V504,
+                             frequencyBodyAccelerationJerkMagnitudeMean = V516,
+                             frequencyBodyAccelerationJerkMagnitudeStd = V517,
+                             frequencyBodyGyroscopeMagnitudeMean = V529,
+                             frequencyBodyGyroscopeMagnitudeStd = V530,
+                             frequencyBodyGyroscopeJerkMagnitudeMean = V542,
+                             frequencyBodyGyroscopeJerkMagnitudeStd = V543)
+        
         ## groups raw_mage df by subject and activity
         sub_act_grp <- group_by(raw_mge_df, subject, activity) 
         ## Summerises the means of all column variables by subject and activity
-        tidy_df <- summarise(sub_act_grp, across(.cols = V1:V543, .fns = mean))
+        tidy_df <- summarise(sub_act_grp, across(.cols = 
+        timeBodyAccelerationXaxisMean:frequencyBodyGyroscopeJerkMagnitudeStd, 
+        .fns = mean))
         tidy_df<- as.data.frame(tidy_df)
                 
         
